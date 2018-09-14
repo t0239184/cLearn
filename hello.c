6fd918706d9ef555f42b045ba7eda29c933ae243
Alan\ Chen <t0239184ps@gmail.com>
2018-09-15 01:55:43 +0800

    first commit helloWorld program!!

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //
+
+/**
+ * Hello World
+ * author:Alan
+ * Time  :2018-09-15
+ */
+int main(void){
+    printf("Hello, First Hello World on Linux!!\n");
+    printf("Character: %c\nInteger: %d\nFloat: %f",'C',24,3.14);
+    return 0;//return 0 equals sucess!!
+}
